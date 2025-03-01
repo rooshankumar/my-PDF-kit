0d230b7d205439211ac9b01fbb17ce4ee2d88b82
rooshankumar <isthisroshan@gmail.com>
2025-03-01 03:48:24 +0000

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 import { Header } from "@/components/shared/Header"
 import { Footer } from "@/components/shared/Footer"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GoogleAnalytics } from '@/components/GoogleAnalytics'
 import { GA_TRACKING_ID } from '@/lib/analytics'
 
@@ -57,6 +59,8 @@ export default function RootLayout({
             <Footer />
           </div>
           <Toaster />
+          <Analytics />
+          <SpeedInsights />
           <GoogleAnalytics />
         </ThemeProvider>
       </body>
